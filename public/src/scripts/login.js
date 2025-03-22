d40f11228a4ddf363ad454171f6e712645b9d94a
Theus03 <matheusssleite@outlook.com>
2025-03-22 17:53:45 -0300

    Route link to home in action in login of app

---
+++ public/src/scripts/login.js
@@ -0,0 +1,6 @@
+// URL
+if (document.location.host != "theus03.github.io") {
+    document.getElementById("linkHome").action = "/index.html";
+} else {
+    document.getElementById("linkHome").action = "/University/";
+}
